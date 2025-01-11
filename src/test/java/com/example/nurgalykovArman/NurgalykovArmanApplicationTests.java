da64e78917d64983350c9ea473053b26b8fd24c8
NurgalykovArman <108484666+NurgalykovArman@users.noreply.github.com>
2025-01-11 19:23:25 +0500

    init/project-setup

---
+++ src/test/java/com/example/nurgalykovArman/NurgalykovArmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nurgalykovArman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurgalykovArmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
